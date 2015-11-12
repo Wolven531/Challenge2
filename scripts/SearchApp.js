e018acbae95839f7e295bfb975a661df807cea1d
Anthony Williams <anthony.williams.cs@gmail.com>
2015-11-11 18:23:28 -0800

    Got URL update working with query params for search and page; added no data row to table

--- scripts/SearchApp.js
+++ scripts/SearchApp.js
@@ -1,18 +1,15 @@
 'use strict';
 
 angular.module('searchApp', ['ngTable'])
-  .controller('SearchController', function($scope, $http, NgTableParams) {
+  .controller('SearchController', function($scope, $http, NgTableParams, $location) {
     var searcher = this;
     var productEndpoint = 'http://api.vip.supplyhub.com:19000/products';
-    $scope.query = '';
-    $scope.limit = 10;
-    $scope.skip = 0;
+    $scope.query = $location.search().query || '';
     $scope.results = [];
 
     function getTableParams(rows) {
-      console.log('getTableParams()');
       var tableParamConfig = {
-        page: 1,
+        page: $location.search().page,
         count: 10//,
         // sorting: {
           // name: 'asc'
@@ -23,17 +20,21 @@ angular.module('searchApp', ['ngTable'])
         // counts: [10, 25, 50, 100],// shows buttons for how many to display at a time
         dataset: rows
       };
-      // is there a better way to get table params than new everytime?
-      return new NgTableParams(tableParamConfig, tableSettingsConfig);
+      var pars = new NgTableParams(tableParamConfig, tableSettingsConfig);
+      var orig_page = pars.page;
+      pars.page = function(pageNum) {// to hook into the page change
+        var result = orig_page.call(pars, pageNum);
+        if(!isNaN(pageNum)) {// to make sure this is page change, not retrieval
+          $location.search({ query: $scope.query, page: this.page() });
+        }
+        return result;
+      };
+      return pars;
     }
 
-    function endpointErr(resp, $defer) {// failure callback
+    function endpointErr(resp) {// failure callback
       if(resp.status === 404) {// no results were returned, as per REST api
         $scope.results = [];
-      }
-      if($defer) {
-        $defer.resolve($scope.results);
-        return;
       }
       searcher.tableParams.settings({ dataset: $scope.results });
     }
@@ -45,18 +46,13 @@ angular.module('searchApp', ['ngTable'])
         return 0;
       }
       return (((searcher.tableParams.page() - 1) * searcher.tableParams.count()) + 1)
-        + ' - '
-        + searcher.tableParams.page() * searcher.tableParams.count()
-        + ' of ' + searcher.tableParams.total();
+        + ' - ' + searcher.tableParams.page() * searcher.tableParams.count() + ' (of ' + searcher.tableParams.total() + ')';
     };
 
-    $scope.runSearch = function($defer, params) {// happens on search change or table update
+    $scope.runSearch = function(retainPage) {// happens on search change and pageload
+      $location.search({ query: $scope.query, page: searcher.tableParams.page() });
       if($scope.query.length === 0) {// don't search without required param
         $scope.results = [];
-        if($defer) {
-          $defer.resolve($scope.results);
-          return;
-        }
         searcher.tableParams.settings({ dataset: $scope.results });
         return;
       }
@@ -67,29 +63,22 @@ angular.module('searchApp', ['ngTable'])
           count: 1// if true, returns count instead
         }
       };
-      if(params) {// overwrite values if there are new settings
-        apiConfig.params.limit = params.count();
-        apiConfig.params.skip = (params.page() - 1) * params.count();
-      }
       $http.get(productEndpoint, apiConfig)// grab totals first
         .then(function(resp) {
           var total = resp.data.count;
-          console.log('Total available: ' + total);
           delete apiConfig.params.count;// switch from counting endpoint
           apiConfig.params.limit = total || 1;// add the limit in, default to 1 if no results to avoid 400
           return $http.get(productEndpoint, apiConfig);
-        }, function(resp) {
-          endpointErr(resp, $defer);
-        })
+        }, endpointErr)
         .then(function(resp) {// grab actual results
           $scope.results = resp.data;
-          if($defer) {
-            $defer.resolve($scope.results);
-            return;
-          }
+          var page = searcher.tableParams.page();
           searcher.tableParams.settings({ dataset: $scope.results });
-        }, function(resp) {
-          endpointErr(resp, $defer);
-        });
+          if(retainPage) {
+            searcher.tableParams.page(page);
+          }
+        },  endpointErr);
     };
+
+    $scope.runSearch(true);// first search for preloaded query params
   });
